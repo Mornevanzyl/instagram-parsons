acde9c57efbb51ecf10dabd5a6d7324319a18005
Morne van Zyl <mornevanzyl@mornes-mac-mini.home>
2021-04-16 08:07:15 +0100

    Complete login slider window animation

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookF, faGooglePlusG, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
@@ -8,15 +8,21 @@ export default function Signup() {
     const passwordRef = useRef();
     const passwordConfirmRef = useRef();
 
+    const [rightPanelActive, setRightPanelActive] = useState(false);
+
+    function handleSlider() {
+        setRightPanelActive(!rightPanelActive);
+    }
+
     return (
-        <div class="container" id="container">
-            <div class="form-container sign-up-container">
+        <div className={`container ${rightPanelActive ? 'right-panel-active' : ''}`} id="container">
+            <div className={"form-container sign-up-container"}>
                 <form action="#">
                     <h1>Create Account</h1>
-                    <div class="social-container">
-                        <a href="#" class="social"><FontAwesomeIcon icon={faFacebookF} /></a>
-                        <a href="#" class="social"><FontAwesomeIcon icon={faGooglePlusG} /></a>
-                        <a href="#" class="social"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                    <div class={"social-container"}>
+                        <a href="#" className={"social"}><FontAwesomeIcon icon={faFacebookF} /></a>
+                        <a href="#" className={"social"}><FontAwesomeIcon icon={faGooglePlusG} /></a>
+                        <a href="#" className={"social"}><FontAwesomeIcon icon={faLinkedinIn} /></a>
                     </div>
                     <span>or use your email for registration</span>
                     <input type="text" placeholder="Name" ref={nameRef} />
@@ -26,13 +32,13 @@ export default function Signup() {
                     <button type="submit" >Sign Up</button>
                 </form>
             </div>
-            <div class="form-container sign-in-container">
+            <div className={"form-container sign-in-container"}>
                 <form action="#">
                     <h1>Sign in</h1>
-                    <div class="social-container">
-                        <a href="#" class="social"><FontAwesomeIcon icon={faFacebookF} /></a>
-                        <a href="#" class="social"><FontAwesomeIcon icon={faGooglePlusG} /></a>
-                        <a href="#" class="social"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                    <div className={"social-container"}>
+                        <a href="#" className={"social"}><FontAwesomeIcon icon={faFacebookF} /></a>
+                        <a href="#" className={"social"}><FontAwesomeIcon icon={faGooglePlusG} /></a>
+                        <a href="#" className={"social"}><FontAwesomeIcon icon={faLinkedinIn} /></a>
                     </div>
                     <span>or use your account</span>
                     <input type="email" placeholder="Email" ref={emailRef} required />
@@ -41,17 +47,17 @@ export default function Signup() {
                     <button>Sign In</button>
                 </form>
             </div>
-            <div class="overlay-container">
-                <div class="overlay">
-                    <div class="overlay-panel overlay-left">
+            <div className={"overlay-container"}>
+                <div className={"overlay"}>
+                    <div className={"overlay-panel overlay-left"}>
                         <h1>Welcome Back!</h1>
                         <p>To keep connected with us please login with your personal info</p>
-                        <button class="ghost" id="signIn">Sign In</button>
+                        <button className={"ghost"} id="signIn" onClick={handleSlider}>Sign In</button>
                     </div>
-                    <div class="overlay-panel overlay-right">
+                    <div className={"overlay-panel overlay-right"}>
                         <h1>Hello, Friend!</h1>
                         <p>Enter your personal details and start journey with us</p>
-                        <button class="ghost" id="signUp">Sign Up</button>
+                        <button className={"ghost"} id="signUp" onClick={handleSlider}>Sign Up</button>
                     </div>
                 </div>
             </div>
