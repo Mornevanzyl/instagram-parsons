de84726c0255dcbb6c1829b29ce53c6ae60465ac
Morne van Zyl <mornevanzyl@Mornes-Mac-mini.local>
2021-04-20 20:59:46 +0100

    Added image modal

--- src/components/photos/Dashboard.jsx
+++ src/components/photos/Dashboard.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ImageGrid from './ImageGrid'
+import Modal from './Modal'
 import Navbar from './Navbar'
 import UploadForm from './UploadForm'
 
 export default function Dashboard() {
+  const [selectedImage, setSelectedImage] = useState(null);
   return (
     <div className="container">
       <div className="photo-container">
         <Navbar />
         <UploadForm />
-        <ImageGrid />
+        <ImageGrid setSelectedImage={setSelectedImage} />
+        { selectedImage && <Modal selectedImage={selectedImage} setSelectedImage={setSelectedImage} /> }
       </div>
     </div>
   )
---
+++ src/components/photos/Modal.jsx
@@ -0,0 +1,14 @@
+export default function Modal({ selectedImage, setSelectedImage }) {
+
+  function handleClick(e) {
+    if (e.target.classList.contains('backdrop')) {
+      setSelectedImage(null);
+    };
+  }
+
+  return (
+    <div className="backdrop" onClick={handleClick} >
+      <img src={selectedImage} alt="enlarged pic" />
+    </div>
+  )
+}
